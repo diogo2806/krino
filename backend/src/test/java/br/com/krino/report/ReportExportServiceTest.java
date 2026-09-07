c429a0ecdfd7c69f81babf1f0329bca7b8ee961b
Diogo Santos <diogo_2806@hotmail.com>
2026-09-07 16:44:13 -0300

    test: cobrir escopo de estudante nas exportações

--- backend/src/test/java/br/com/krino/report/ReportExportServiceTest.java
+++ backend/src/test/java/br/com/krino/report/ReportExportServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.security.core.Authentication;
 
 import br.com.krino.report.AssessmentReportService.BreakdownRow;
+import br.com.krino.report.AssessmentReportService.InterventionProfile;
 
 class ReportExportServiceTest {
 
@@ -20,8 +21,9 @@ class ReportExportServiceTest {
     void shouldExportSchoolParticipationBrokenDownByClassAsCsv() {
         AssessmentReportService reportService = mock(AssessmentReportService.class);
         ReportAccessService accessService = mock(ReportAccessService.class);
+        ReportFilterService filterService = mock(ReportFilterService.class);
         Authentication authentication = mock(Authentication.class);
-        ReportExportService service = new ReportExportService(reportService, accessService, new CsvWriter());
+        ReportExportService service = new ReportExportService(reportService, accessService, filterService, new CsvWriter());
         when(reportService.participation(10L, "CLASS", 2L, null, authentication)).thenReturn(List.of(
                 new BreakdownRow(7L, "Turma A", 8L, 10L, new BigDecimal("80.00"))));
 
@@ -33,5 +35,21 @@ class ReportExportServiceTest {
                 .contains("Nível", "Turma A", "8", "10", "80.00");
         verify(accessService).requireExport(authentication, 2L);
         verify(reportService).participation(10L, "CLASS", 2L, null, authentication);
+    }
+
+    @Test
+    void shouldValidateStudentScopeBeforeInterventionExport() {
+        AssessmentReportService reportService = mock(AssessmentReportService.class);
+        ReportAccessService accessService = mock(ReportAccessService.class);
+        ReportFilterService filterService = mock(ReportFilterService.class);
+        Authentication authentication = mock(Authentication.class);
+        ReportExportService service = new ReportExportService(reportService, accessService, filterService, new CsvWriter());
+        when(reportService.intervention(10L, 30L, 2L, 7L, authentication)).thenReturn(
+                new InterventionProfile(30L, "MAT-30", "Estudante Teste", new BigDecimal("75.00"), "Adequado", List.of(), List.of()));
+
+        service.export(10L, "INTERVENTION", 2L, 7L, 30L, authentication);
+
+        verify(filterService).requireStudentVisible(10L, 30L, 2L, 7L, authentication);
+        verify(reportService).intervention(10L, 30L, 2L, 7L, authentication);
     }
 }

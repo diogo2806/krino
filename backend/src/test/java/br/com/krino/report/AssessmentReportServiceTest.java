aa9e3b39d8059a821e2c4732fff561d1c47357ae
Diogo Santos <diogo_2806@hotmail.com>
2026-09-07 16:40:21 -0300

    test: validar memória de cálculo dos relatórios

---
+++ backend/src/test/java/br/com/krino/report/AssessmentReportServiceTest.java
@@ -0,0 +1,33 @@
+package br.com.krino.report;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+class AssessmentReportServiceTest {
+
+    @Test
+    void shouldCalculateSeventyFivePercentWithTwoDecimalPlaces() throws Exception {
+        AssessmentReportService service = new AssessmentReportService(null, null, null);
+        Method percentage = AssessmentReportService.class.getDeclaredMethod("percentage", long.class, long.class);
+        percentage.setAccessible(true);
+
+        BigDecimal result = (BigDecimal) percentage.invoke(service, 150L, 200L);
+
+        assertThat(result).isEqualByComparingTo(new BigDecimal("75.00"));
+    }
+
+    @Test
+    void shouldReturnNullWhenPercentageBaseIsZero() throws Exception {
+        AssessmentReportService service = new AssessmentReportService(null, null, null);
+        Method percentage = AssessmentReportService.class.getDeclaredMethod("percentage", long.class, long.class);
+        percentage.setAccessible(true);
+
+        BigDecimal result = (BigDecimal) percentage.invoke(service, 10L, 0L);
+
+        assertThat(result).isNull();
+    }
+}
